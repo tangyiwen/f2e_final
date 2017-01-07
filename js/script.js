4b2109c4721c9e8799876c3144dd3fc1b3e00c8f
tangyiwen <fa33053@gmail.com>
2017-01-07 22:31:15 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("img.search_btn").click(function(){     /*search fade in/out*/
-    $("input.search_bar").fadeToggle(400);
+    $("#___gcse_0").fadeToggle(400);
   });
 
 
@@ -37,6 +37,24 @@ $(document).ready(function(){
     $("html").css({overflow:"auto"});
   });
 
+  (function() { /*google search*/
+    var cx = '011408406026343633144:ugb1pksvhoa';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ /*google-analytics*/
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89930154-1', 'auto');
+  ga('send', 'pageview');
 
 
 
